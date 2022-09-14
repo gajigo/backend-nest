9d538d580b66af7be62ecdf2f39f383a32d711c0
Pedro Henrique Garcia <pehdfms@gmail.com>
2022-09-14 20:25:31 -0300

    feat: add current entities to scaffolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
+import { DocumentsModule } from './documents/documents.module'
+import { EventsModule } from './events/events.module'
+import { LecturesModule } from './lectures/lectures.module'
+import { LanguagesModule } from './languages/languages.module'
+import { RoomsModule } from './rooms/rooms.module'
 
 @Module({
-  imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
+  imports: [
+    UsersModule,
+    DocumentsModule,
+    EventsModule,
+    LecturesModule,
+    LanguagesModule,
+    RoomsModule
+  ]
 })
 export class AppModule {}
